f451eb39c13692950d5ddcc1943c41cf7ebdfa7f
patrick Akuagwu <57248301+patrick-hub@users.noreply.github.com>
2022-04-08 14:28:13 -0700

    Update 101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,54 +1,47 @@
 #include <stdio.h>
-#include <stdlib.h>
+
 /**
- * main - main block
- * Description: Write a program that prints all possible
- * different combinations of two digits
- * Numbers must be separated by ,, followed by a space
- * The two digits must be different
- * 01 and 10 are considered the same combination of the two digits 0 and 1
- * Print only the smallest combination of two digits
- * Numbers should be printed in ascending order, with two digits
- * You can only use the putchar function
- * (every other function (printf, puts, etc…) is forbidden)
- * You can only use putchar five times maximum in your code
- * You are not allowed to use any variable of type char
- * All your code should be in the main function
- * Return: 0
+ * main - print triple combos
+ *
+ * Description: print triple digit combos
+ *
+ * Return: Always 0 (Success)
  */
+
 int main(void)
 {
-	int c;
-	int d;
-	int e = 0;
+	int i, j, k;
 
-	while (e < 10)
+	i = 48;
+	j = 48;
+	k = 48;
+
+	while (i < 58)
 	{
-		d = 0;
-		while (d < 10)
+		j = i + 1;
+		while (j < 58)
 		{
-			c = 0;
-			while (c < 10)
-			{
-				if (c != d && d != e && e < d && d < c)
+			k = j + 1;
+				while (k < 58)
 				{
-					putchar('0' + e);
-					putchar('0' + d);
-					putchar('0' + c);
-
-					if (c + d + e != 9 + 8 + 7)
+					putchar(i);
+					putchar(j);
+					putchar(k);
+					if (i < 55 || j < 56 || k < 57)
 					{
-						putchar(",");
-						putchar(' ');
+						putchar(44);
+						putchar(32);
 					}
+					k++;
 				}
 
-				c++;
-			}
-			d++;
+			j++;
 		}
-		e++;
+
+		i++;
 	}
-	putchar('');
+
+	putchar(10);
+
 	return (0);
 }
